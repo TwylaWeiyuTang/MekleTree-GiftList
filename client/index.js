ad7ba4a799e5b42d1b7db6b3304e8834b51358b8
Weiyu Tang <twyla9876@outlook.com>
2023-04-27 16:30:27 +0100

    passed name and proof to server, added merkle root, and verifyProof to check if passed in name and proof is inside the merkle tree

--- client/index.js
+++ client/index.js
@@ -1,14 +1,24 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
+
+const merkleTree = new MerkleTree(niceList);
+
+const root = merkleTree.getRoot();
+console.log(root);
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?
+  const name = "Ruby Stark";
+  const index = niceList.findIndex((n) => n === name);
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name: name,
+    proof: proof,
   });
 
   console.log({ gift });
